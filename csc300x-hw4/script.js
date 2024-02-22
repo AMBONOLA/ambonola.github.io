a5a4acbd5f7650cc0377303442f1a399fde6d2e4
ambonola <andreabonola@gmail.com>
2024-02-22 10:52:10 -0500

    WIP

--- csc300x-hw4/script.js
+++ csc300x-hw4/script.js
@@ -1,4 +1,5 @@
 const imgArray = document.querySelectorAll('img');
+
 imgArray.forEach(img => {
   if (img.parentElement.classList.contains('image-container')) {
     img.addEventListener('click', addRedBorder);
@@ -10,6 +11,14 @@ imgArray.forEach(img => {
 function addRedBorder(event) {
   const img = event.currentTarget;
   img.style.border = "5px solid red";
+  // after a selection is made we can make it so other items can't be clicked
+  imgArray.forEach(otherImg => {
+    if (otherImg.parentElement.classList.contains('image-container') && otherImg !== img) {
+      otherImg.removeEventListener('click', addRedBorder)
+      otherImg.style.cursor = "auto"
+    }
+  })
+
 }
 
 function addBlueBorder(event) {
